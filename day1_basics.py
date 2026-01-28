11ac7fb404fa962c5b59006af5abc0d90c4191ab
zainab mairaj <zmairaj16@gmail.com>
2026-01-28 19:06:53 +0500

    Add files via upload

---
+++ day1_basics.py
@@ -0,0 +1,23 @@
+#Q1: Store your name, age, and city in variables and print them.
+name="Zainab"
+age= 20
+city= "hyderabad"
+print(name)
+print(age)
+print(city)
+#Q2: Take two numbers from user and print their sum.
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+sum= a+b
+print(sum)
+#Q3: Take a number from user and print its data type.
+a= int(input("Enter a number: "))
+print(type(a))
+#Q4: Convert a float to int and print both values.
+floatnum = 2.89
+intnum = int(floatnum)
+print("float value: ", floatnum)
+print("integer value: ", intnum)
+#Q5: Take user name and print: Welcome, <name>!
+str = input("Enter your name: ")
+print("Welcome", str)
